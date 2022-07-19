2c1820244b2ed7e6b4eb20ed35fbb4e750198574
madebyTimo <t.schnaible@madebytimo.de>
2022-07-20 00:37:16 +0200

    change script to not recreate runner

--- start-actions-runner.sh
+++ start-actions-runner.sh
@@ -4,14 +4,9 @@ echo url=$url
 echo token=$token
 echo name=$name
 
-./config.sh --unattended --url $url --token $token --name $name
+if [ ! -f "configured" ]; then
+    ./config.sh --unattended --url $url --token $token --name $name
+    touch "configured"
+fi
 
-cleanup() {
-    echo "Removing runner..."
-    ./config.sh remove --unattended --token $token
-}
-
-trap 'cleanup; exit 130' INT
-trap 'cleanup; exit 143' TERM
-
-./run.sh & wait $!
+./run.sh
