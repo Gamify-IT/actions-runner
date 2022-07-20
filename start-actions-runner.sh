c7149f78ff39c6e7c7a4b1a27107ee3a080f5aab
madebyTimo <t.schnaible@madebytimo.de>
2022-07-20 23:52:10 +0200

    readd docker socket from host and add shared work dir

--- start-actions-runner.sh
+++ start-actions-runner.sh
@@ -3,11 +3,11 @@
 echo url=$url
 echo token=$token
 echo name=$name
+echo workdir=$workdir
 
 if [ ! -f "configured" ]; then
-    ./config.sh --unattended --url $url --token $token --name $name
+    ./config.sh --unattended --url $url --token $token --name $name --work $workdir
     touch "configured"
 fi
 
-dockerd &
 ./run.sh
